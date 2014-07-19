14c8c67c6cd4c9c4435af8364a8feb65b77da6a4
susu <susu_mm@126.com>
2014-07-19 18:16:05 +0800

    Initial Commit

---
+++ MfeijiTests/MfeijiTests.m
@@ -0,0 +1,34 @@
+//
+//  MfeijiTests.m
+//  MfeijiTests
+//
+//  Created by susu on 14-7-19.
+//  Copyright (c) 2014年 susu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MfeijiTests : XCTestCase
+
+@end
+
+@implementation MfeijiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
